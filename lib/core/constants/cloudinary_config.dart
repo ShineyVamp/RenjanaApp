dc22ddc3a60ff4c6eda26581019aaaef1cd05156
ShineyVamp <shineyvampire@gmail.com>
2026-09-11 13:42:06 +0700

    push meong main

--- lib/core/constants/cloudinary_config.dart
+++ lib/core/constants/cloudinary_config.dart
@@ -1,10 +1,14 @@
+import 'cloudinary_keys.dart';
+
 class CloudinaryConfig {
   CloudinaryConfig._();
-  static const String cloudName = 'yykabu7v';
-  static const String uploadPreset = 'renjana_preset';
-  static const String defaultFolder = 'renjana';
-  static String get uploadUrl =>
-      'https://api.cloudinary.com/v1_1/$cloudName/image/upload';
+
+  static const String cloudName = CloudinaryKeys.cloudName;
+  static const String uploadPreset = CloudinaryKeys.uploadPreset;
+  static const String defaultFolder = CloudinaryKeys.defaultFolder;
+
+  static String get uploadUrl => CloudinaryKeys.baseUrl;
+
   static bool get isConfigured =>
       cloudName.isNotEmpty &&
       cloudName != 'YOUR_CLOUD_NAME' &&
